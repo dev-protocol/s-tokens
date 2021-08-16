4300aaa83169eb80bbe5bac2c63905f53571ef2e
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-08-16 15:31:23 +0900

    update Base64

--- contracts/Base64.sol
+++ contracts/Base64.sol
@@ -1,75 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 pragma solidity 0.8.7;
 
-// library Base64 {
-
-//     bytes constant private base64stdchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-//     bytes constant private base64urlchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
-//     function encode(string memory _str) internal pure returns (string memory) {
-
-//         bytes memory _bs = bytes(_str);
-//         uint256 rem = _bs.length % 3;
-
-//         uint256 res_length = (_bs.length + 2) / 3 * 4 - ((3 - rem) % 3);
-//         bytes memory res = new bytes(res_length);
-
-//         uint256 i = 0;
-//         uint256 j = 0;
-
-//         for (; i + 3 <= _bs.length; i += 3) {
-// 			bytes memory tmp = encode3(
-//                 uint8(_bs[i]),
-//                 uint8(_bs[i+1]),
-//                 uint8(_bs[i+2])
-//             );
-// 			res[j] = tmp[0];
-// 			res[j+1] = tmp[1];
-// 			res[j+2] = tmp[2];
-// 			res[j+3] = tmp[3];
-
-//             j += 4;
-//         }
-
-//         if (rem != 0) {
-//             uint8 la0 = uint8(_bs[_bs.length - rem]);
-//             uint8 la1 = 0;
-
-//             if (rem == 2) {
-//                 la1 = uint8(_bs[_bs.length - 1]);
-//             }
-// 			bytes memory tmp = encode3(la0, la1, 0);
-//             res[j] = tmp[0];
-//             res[j+1] = tmp[1];
-//             if (rem == 2) {
-//               res[j+2] = tmp[2];
-//             }
-//         }
-
-//         return string(res);
-//     }
-
-//     function encode3(uint256 a0, uint256 a1, uint256 a2)
-//         private
-//         pure
-//         returns (bytes memory)
-//     {
-
-//         uint256 n = (a0 << 16) | (a1 << 8) | a2;
-
-//         uint256 c0 = (n >> 18) & 63;
-//         uint256 c1 = (n >> 12) & 63;
-//         uint256 c2 = (n >>  6) & 63;
-//         uint256 c3 = (n      ) & 63;
-// 		bytes memory asciiBytes = new bytes(4);
-// 		asciiBytes[0] = base64urlchars[c0];
-// 		asciiBytes[1] = base64urlchars[c1];
-// 		asciiBytes[2] = base64urlchars[c2];
-// 		asciiBytes[3] = base64urlchars[c3];
-// 		return asciiBytes;
-//     }
-
-// }
 
 library Base64 {
     string internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
@@ -86,6 +17,7 @@ library Base64 {
         // add some extra buffer at the end required for the writing
         string memory result = new string(encodedLen + 32);
 
+		// solhint-disable-next-line no-inline-assembly
         assembly {
             // set the actual output length
             mstore(result, encodedLen)
