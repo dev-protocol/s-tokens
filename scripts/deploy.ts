5f5873f5873d8b8592fc4265734cd1f3531d925d
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-08-25 12:53:56 +0900

    finish to create deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,23 +4,28 @@ import { ethers } from 'hardhat'
 
 async function main() {
 	//!please check!!!!!!!!!
-	const configAddress = '0xD6D07f1c048bDF2B3d5d9B6c25eD1FC5348D0A70'
+	const configAddress = ''
 	//!!!!!!!!!!!!!!!!!!!!!!
 	const sTokensManagerFactory = await ethers.getContractFactory(
 		'STokensManager'
 	)
 	const sTokensManager = await sTokensManagerFactory.deploy()
+	await sTokensManager.deployed()
+	console.log(1)
 	await sTokensManager.initialize(configAddress)
-
+	console.log(2)
 	const sTokensManagerProxyFactory = await ethers.getContractFactory(
-		'STokensManager'
+		'STokensManagerProxy'
 	)
+	console.log(3)
 	const data = ethers.utils.arrayify('0x')
+	console.log(4)
 	const sTokensManagerProxy = await sTokensManagerProxyFactory.deploy(
 		sTokensManager.address,
 		data
 	)
-
+	await sTokensManagerProxy.deployed()
+	console.log(5)
 	console.log('sTokensManager deployed to:', sTokensManager.address)
 	console.log(
 		'sTokensManagerProxyFactory deployed to:',
