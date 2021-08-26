d7ef881c494963682a4e2451f2fa044438d43960
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-08-26 11:31:33 +0900

    add ropsten settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,22 @@
 import '@nomiclabs/hardhat-waffle'
 
+const privateKey = ''
+
 module.exports = {
 	mocha: {
 		timeout: 300000,
 	},
 	solidity: '0.8.4',
+	networks: {
+		ropsten: {
+			url: `https://hogehoge`,
+			accounts: [`0x${privateKey}`],
+		},
+		mainnet: {
+			accounts: [`0x${privateKey}`],
+			url: `https://hogehoge`,
+		},
+	},
 	settings: {
 		optimizer: {
 			enabled: true,
